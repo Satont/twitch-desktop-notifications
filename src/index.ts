cb1746f20c2c04be0e02e2805a6ddd0d3d40e0e5
Satont <satontworldwide@gmail.com>
2022-12-30 14:02:00 +0300

    chore: change messages

--- src/index.ts
+++ src/index.ts
@@ -10,14 +10,14 @@ const me = await apiClient.users.getMe()
 
 await eventSub.subscribeToChannelFollowEvents(me.id, (event) => {
   notifier.notify({
-    title: `New Follow!`,
+    title: `New Follow`,
     message: event.userName
   })
 })
 
 await eventSub.subscribeToChannelRaidEventsTo(me.id, (event) => {
   notifier.notify({
-    title: 'New Raid!',
+    title: 'New Raid',
     message: `${event.raidedBroadcasterName} with ${event.viewers}`
   })
 })
