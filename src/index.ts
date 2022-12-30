a8aed305fa14b13f75bab029ed70f488c29547ca
Satont <satontworldwide@gmail.com>
2022-12-30 14:14:06 +0300

    feat: add icon to notifications

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,20 @@ import notifier from 'node-notifier'
 import './libs/config.js'
 import { eventSub } from "./libs/twitch/eventsub.js";
 import { apiClient } from "./libs/twitch/api.js";
+import * as os from "node:os";
+import { resolve } from "node:path";
 
 await eventSub.start()
 
+const icon = resolve(os.homedir(), 'Pictures', 'twitch.png')
 const me = await apiClient.users.getMe()
 
 await eventSub.subscribeToChannelFollowEvents(me.id, (event) => {
   notifier.notify({
     title: `New Follow`,
     message: event.userName,
-    time: 10000,
+    icon,
+    timeout: 10
   })
 })
 
@@ -20,7 +24,8 @@ await eventSub.subscribeToChannelRaidEventsTo(me.id, (event) => {
   notifier.notify({
     title: 'New Raid',
     message: `${event.raidingBroadcasterName} with ${event.viewers}`,
-    time: 10000,
+    icon,
+    timeout: 10,
   })
 })
 
